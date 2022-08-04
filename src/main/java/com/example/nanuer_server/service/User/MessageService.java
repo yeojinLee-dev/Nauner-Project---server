6f05526ec947f24162bd274945ce21bda3484566
kidzero00 <dkdud203@naver.com>
2022-08-04 16:43:23 +0900

    ms

--- src/main/java/com/example/nanuer_server/service/User/MessageService.java
+++ src/main/java/com/example/nanuer_server/service/User/MessageService.java
@@ -35,7 +35,7 @@ public class MessageService {
         params.put("to", phone);    // 수신전화번호
         params.put("from", fromNumber);    // 발신전화번호. 테스트시에는 발신,수신 둘다 본인 번호로 하면 됨
         params.put("type", "SMS");
-        params.put("text", "나누어 휴대폰인증 테스트 메시지 : 인증번호는" + "["+cerNum+"]" + "입니다.");
+        params.put("text", "나누어 휴대폰인증 메시지 : 인증번호는" + "["+cerNum+"]" + "입니다.");
         params.put("app_version", "test app 1.2"); // application name and version
 
         try {
