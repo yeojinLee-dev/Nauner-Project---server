6a5cf6f0b49020c4fb99a00a989ad3c77dc39329
yeojinLee-dev <yeojinlee.dev@gmail.com>
2022-08-18 23:29:11 +0900

    merge aborting..

--- src/main/java/com/example/nanuer_server/domain/repository/Chat/ChatRoomRepository.java
+++ src/main/java/com/example/nanuer_server/domain/repository/Chat/ChatRoomRepository.java
@@ -4,10 +4,10 @@ import com.example.nanuer_server.domain.entity.ChatRoomEntity;
 import com.example.nanuer_server.domain.entity.PostEntity;
 import com.example.nanuer_server.domain.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.*;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoomEntity, String> {
     Optional<ChatRoomEntity> findByRoomId(String roomId);
-
 }
